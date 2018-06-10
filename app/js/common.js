c8e81d891624911fad4bc5b39ed74152e817733b
Ivan <ivan.incweb@gmail.com>
2018-06-10 12:21:13 +0300

    first-production

--- app/js/common.js
+++ app/js/common.js
@@ -1,19 +1,37 @@
 $(function() {
 	$(window).on('load', function() {
 		$('#preloader').find('div').delay(1000).fadeOut().end().delay(1000).fadeOut('slow');
+		setTimeout(function() {
+			$('#preloader').css('display', 'none');
+		}, 1000);
 	});
 
 	$(".open-link").click(function(e) {
 		e.preventDefault();
 		$(".mobile-menu").toggleClass("opened");
 	});
+	$(".main").click(function(e) {
+		$(".mobile-menu").removeClass("opened");
+	});
 
 	$(document).ready(function() {
 		$(".owl-pictures").owlCarousel({
 			items:1,
 			loop: true,
-			dots: true
+			dots: true,
+			autoplay: true
 		});
 	});
+
+	$("#form").submit(function() {
+        var th = $(this);
+        $.ajax({type: "POST", url: "../mail.php", data: th.serialize()}).done(function() {
+            alert("Спасибо!");
+            setTimeout(function() {
+                th.trigger("reset");
+            }, 1000);
+        });
+        return false;
+    });
 
 });
